dfc387709425518a7a51218e1425cd8a46558c61
Rafael B. Tauil <rafael@tauil.com.br>
2017-08-16 20:36:50 -0300

    Removes mocked matches

--- src/app/modules/match/services/match.service.ts
+++ src/app/modules/match/services/match.service.ts
@@ -1,15 +1,12 @@
 import { Injectable } from '@angular/core';
 import { Match } from '../models/match';
 
-const MATCHES = [ {id: 1}, {id: 2}, {id: 3} ];
-
 @Injectable()
 export class MatchService {
 
-    matches: Array<Match>;
+    matches: Array<Match> = [];
 
     constructor() {
-        this.matches = MATCHES.map((matchAttributes) => new Match(matchAttributes));
     }
 
     all() {
@@ -17,7 +14,7 @@ export class MatchService {
     }
 
     create() {
-        let id = this.matches[this.matches.length - 1].id + 1;
+        let id = this.matches.length + 1;
         let match = new Match({id: id});
         this.matches.push(match);
     }
